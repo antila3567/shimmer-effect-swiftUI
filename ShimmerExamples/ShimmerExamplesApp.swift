2ea186cf0bbd5cf997d28e161a334d9b79c5baa5
IvanL <antila3567@gmail.com>
2023-12-26 20:20:32 +0200

    Initial Commit

---
+++ ShimmerExamples/ShimmerExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimmerExamplesApp.swift
+//  ShimmerExamples
+//
+//  Created by Иван Легенький on 26.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShimmerExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
